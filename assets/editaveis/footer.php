77dddae7a52cd63d202d3b19512a79af5c69d6bd
愛 <alissonabreuh@gmail.com>
2024-10-15 17:50:06 -0300

    Delete assets/editaveis directory

--- assets/editaveis/footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-        <img src="../../assets/img/wave.svg" alt="">
-
-        <div id="footer_items">
-            <span id="copyright">
-                &copy 2024 Alisson Abreu
-            </span>
-    </footer>
